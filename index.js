c6464c5f1891d4d08ab6e1e44efa582aa78b3f85
Олександр <moskaletzsanych@gmail.com>
2023-01-13 20:22:18 +0200

    2

---
+++ index.js
@@ -0,0 +1,64 @@
+// const value = 27.9;
+
+// const ceil = Math.ceil(value);
+// const floor = Math.floor(value);
+// const round = Math.round(value);
+
+// console.log(ceil, floor, round)
+
+// const companyName = "Cyberdyne Systems";
+
+// const repairBots = 150;
+
+// const defenceBots = 50;
+
+// const sentence = `${companyName} has ${repairBots + defenceBots} in stock`
+
+// console.log(sentence)
+
+// let a = 5;
+// let b = 3;
+
+// let c;
+
+// function sum(a, b){
+//     const c = a + b;
+//     console.log(c)
+// }
+
+// sum(a, b)
+
+// a = 40;
+// b = 300;
+
+// sum(a, b)
+
+// function myFn(a, b) {
+//     let c;
+//     a += 1;
+//     c = a + b;
+//     return c;
+// }
+
+// console.log(myFn(120, 40))
+
+// function printMyName() {
+//     console.log(`Alex`)
+// }
+
+// setTimeout(printMyName, 1000)
+
+let a;
+let b;
+
+function myFn() {
+    let b;
+    a = true;
+    b = 10;
+    console.log(b);
+}
+
+myFn();
+
+console.log(a);
+console.log(b)
